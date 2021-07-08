cee50c2283bccc1f7e06491c3be303ca129efb40
Vladislav <vladislav.igorevich.workmail@gmail.com>
2021-07-08 23:17:39 +0300

    Added drawing pattern module

---
+++ Assets/Scripts/DrawingPattern.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawingPattern : MonoBehaviour
+{
+    private GameObject cellPrefab, instantiated;
+
+
+    void OnEnable()
+    {
+        Start();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cellPrefab = Resources.Load("Prefabs/Patterns/CellForDrawing") as GameObject;
+        instantiated = Instantiate(cellPrefab, Vector2.zero, Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        var patternPosition = new Vector2((int)mousePos.x, (int)mousePos.y);
+
+        instantiated.transform.position = patternPosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Instantiate(cellPrefab, patternPosition, Quaternion.identity);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destroy(instantiated);
+            this.enabled = false;
+        }
+    }
+}
